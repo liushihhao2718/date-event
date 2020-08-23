8d03d389b950d5caad16423338dacdf22f9a2759
tom liu <liushihhao2718@gmail.com>
2020-08-23 22:33:02 +0800

    UPDATE build

---
+++ build/precache-manifest.c4b1a855cf74b509cc7b11e11316cfa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721c3ca3dae422d7554ec10d3a9e84fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0670593feb5b206ff292",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f4359a7f037da83fa06a",
+    "url": "/static/css/main.8dc6a40f.chunk.css"
+  },
+  {
+    "revision": "0670593feb5b206ff292",
+    "url": "/static/js/2.7f92995f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7f92995f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4359a7f037da83fa06a",
+    "url": "/static/js/main.6d9614a7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
