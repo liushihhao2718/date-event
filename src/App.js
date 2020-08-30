6bc1d258f73151a55d84362c4cba7174683efbb0
Tom1_Liu <tom1_liu@asus.com>
2020-08-30 11:02:11 +0800

    UPDATE eventviews

--- src/App.js
+++ src/App.js
@@ -1,231 +1,70 @@
-import React, { useState } from "react";
-import { Calendar } from "./calendar.js";
-import { Button, Modal, ListGroup } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-let fetchEvents = false;
-function App() {
-	const [date, setDate] = useState(new Date());
-	const [showEvent, setShowEvent] = useState(null);
-	const handleClose = () => setShowEvent(null);
-	const handleShow = (d) => setShowEvent(d);
-	const [events, setEvents] = useState([]);
+import React, {useState} from 'react'
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
-	const buttonStyle = {
-		display: "flex",
-		alignSelf: "center",
-	};
+const App = () => {
 
-	// let events = []
 
-	if(!fetchEvents){
-		fetchEvents = !fetchEvents;
-		fetch('../public/event.json').then(x=>x.json()).then(x=>setEvents(x))
-	}
-	
 
-	return (
-		<>
-			<main className="px-0 px-md-5 pt-3 pb-1 pb-md-5">
-				<div className="calendar">
-					<div className="month-indicator">
-						<Button
-							className="btn-secondary"
-							style={buttonStyle}
-							onClick={() => setDate(changeMonth(date, -1))}
-						>
-							<LeftArrow />
-						</Button>
-						<time>
-							{date.getFullYear()}
-							{" / "}
-							{date.toLocaleString("zh-tw", { month: "long" })}
-						</time>
-						<Button
-							className="btn-secondary"
-							style={buttonStyle}
-							onClick={() => setDate(changeMonth(date, 1))}
-						>
-							<RightArrow />
-						</Button>
-					</div>
+    const [activeItem, setActive] = useState('book')
+    return (
 
-					<MonthTitle />
-					<DayBlock
-						month={makeCalendarArray(date, events)}
-						onClick={(d) => {
-							handleShow(d);
-						}}
-					/>
-				</div>
-			</main>
-			<Modal
-				show={showEvent !== null}
-				onHide={handleClose}
-				className="pb-5"
-				size="lg"
-				aria-labelledby="contained-modal-title-vcenter"
-				centered
-			>
-				<Modal.Header closeButton>
-					<Modal.Title>{formatDate(showEvent)}</Modal.Title>
-				</Modal.Header>
-				<Modal.Body closeButton>
-					<EventDiary
-						events={events.filter(
-							(ev) => ev.date === formatDate(showEvent)
-						)}
-					/>
-				</Modal.Body>
-			</Modal>
-		</>
-	);
-}
+  <div>
+      <Menu fixed='top' inverted>Hello</Menu>
+    <Menu fixed='top' inverted>
+        <Menu.Item
+          name='editorials'
+          active={activeItem === 'book'}
+          onClick={()=>setActive('book')}
+        >
+          Books
+        </Menu.Item>
 
-function EventDiary(props) {
-	const { events } = props;
-	return (
-		<ListGroup>
-			{events.map((ev) => {
-				return (
-					<ListGroup.Item>
-						<p>{ev.title}</p>
-						<p>
-							<a href={ev.link}>{ev.link}</a>
-						</p>
-					</ListGroup.Item>
-				);
-			})}
-		</ListGroup>
-	);
-}
+        <Menu.Item
+          name='reviews'
+          active={activeItem === 'event'}
+          onClick={()=>setActive('event')}
+        >
+          Events
+        </Menu.Item>
 
-/**
- *
- * @param {Date} date
- * @returns {{date:Date,text:string,sameMonth:boolean,hasEvent:boolean}}
- */
-function makeCalendarArray(date, events) {
-	const cal = new Calendar();
+        <Menu.Item
+          name='upcomingEvents'
+          active={activeItem === 'upcomingEvents'}
+          onClick={()=>setActive('book')}
+        >
+          Upcoming Events
+        </Menu.Item>
+    </Menu>
 
-	let m = cal
-		.monthDates(date.getFullYear(), date.getMonth(), (d) => {
-			return {
-				date: d,
-				text: (" " + d.getDate()).slice(-2),
-				sameMonth: date.getMonth() === d.getMonth(),
-				hasEvent: events.map((ev) => ev.date).includes(formatDate(d)),
-			};
-		})
-		.flat();
-	return m;
-}
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as='h1'>Semantic UI React Fixed Template</Header>
+      <p>This is a basic fixed menu template using fixed size containers.</p>
+      <p>
+        A text container is used for the main container, which is useful for single column layouts.
+      </p>
 
-function formatDate(date) {
-	var d = new Date(date),
-		month = "" + (d.getMonth() + 1),
-		day = "" + d.getDate(),
-		year = d.getFullYear();
+      <Image src='/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+    </Container>
 
-	if (month.length < 2) month = "0" + month;
-	if (day.length < 2) day = "0" + day;
-
-	return [year, month, day].join("-");
-}
-
-function changeMonth(d = new Date(), change = 0) {
-	const newDate = new Date(d);
-	if (change !== 0) newDate.setMonth(newDate.getMonth() + change);
-	console.log(newDate.toLocaleDateString());
-	return newDate;
-}
-
-function MonthTitle() {
-	return (
-		<div className="day-of-week">
-			<div>Su</div>
-			<div>Mo</div>
-			<div>Tu</div>
-			<div>We</div>
-			<div>Th</div>
-			<div>Fr</div>
-			<div>Sa</div>
-		</div>
-	);
-}
-
-function DayBlock(props) {
-	const timeStyle = (day) => {
-		return {
-			color: day.sameMonth ? "var(--blue-grey-600)" : "lightgray",
-		};
-	};
-
-	const handleClick = (x) => {
-		if (x.hasEvent) onClick(x.date.toLocaleDateString());
-	};
-
-	const { month, onClick } = props;
-	const days = month.map((x) => {
-		return (
-			<button
-				className={x.hasEvent ? "has-event" : ""}
-				key={x.date.toLocaleDateString()}
-				onClick={() => handleClick(x)}
-			>
-				<time
-					dateTime={x.date.toLocaleDateString()}
-					style={timeStyle(x)}
-				>
-					{x.text}
-				</time>
-			</button>
-		);
-	});
-	return <div className="date-grid">{days}</div>;
-}
-
-function RightArrow() {
-	return (
-		<svg
-			width="1em"
-			height="1em"
-			viewBox="0 0 16 16"
-			className="bi bi-arrow-right"
-			fill="currentColor"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<path
-				fillRule="evenodd"
-				d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z"
-			/>
-			<path
-				fillRule="evenodd"
-				d="M2 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8z"
-			/>
-		</svg>
-	);
-}
-function LeftArrow() {
-	return (
-		<svg
-			width="1em"
-			height="1em"
-			viewBox="0 0 16 16"
-			className="bi bi-arrow-left"
-			fill="currentColor"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<path
-				fillRule="evenodd"
-				d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
-			/>
-			<path
-				fillRule="evenodd"
-				d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-			/>
-		</svg>
-	);
+      
+  </div>)
 }
 
 export default App;
